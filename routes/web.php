67ad53b576feb9daa17f59bf863b756b800f66d9
rupan dangol <rupandangol87@gmail.com>
2025-06-24 11:58:25 +0545

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,14 @@
 
 use App\Http\Controllers\TransactionsController;
 use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    // User::factory()->createOne();
-    // Transaction::factory()->createOne();
-    // $user = Transaction::all();
-    // $transactions = Transaction::all();
-    // return view('welcome');
-    return view('user.dashboard');
-})->name('test');
 
-Route::get('/transactions', [TransactionsController::class, 'index'])->name('transaction.index');
-Route::post('/transactions/import', [TransactionsController::class, 'import'])->name('transaction.import');
+Auth::routes();
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/transactions', [TransactionsController::class, 'index'])->name('transaction.index');
+    Route::post('/transactions/import', [TransactionsController::class, 'import'])->name('transaction.import');
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
