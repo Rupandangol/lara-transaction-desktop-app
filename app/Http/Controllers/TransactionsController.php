115e5d1949e2aefaeff0d83c996afad1f3a6f48a
rupan dangol <rupandangol87@gmail.com>
2025-07-18 11:40:27 +0545

    Percent changes for all the months and graph

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -15,6 +15,8 @@ use Maatwebsite\Excel\Facades\Excel;
 use Native\Laravel\Facades\Alert;
 use Native\Laravel\Facades\Notification;
 
+use function PHPUnit\Framework\isEmpty;
+
 class TransactionsController extends Controller
 {
     public const BASE_AMOUNT = 0;
@@ -33,9 +35,12 @@ class TransactionsController extends Controller
         if ($request->filled('description')) {
             $query->where('description', 'like', '%' . $request->get('description') . '%');
         }
-
-
-        $nowYear = Carbon::now()->format('Y');
+        if ($request->filled('year')) {
+            $query->whereYear('date_time', Carbon::parse($request->get('year'))->format('Y'));
+            $nowYear = $request->get('year');
+        } else {
+            $nowYear = Carbon::now()->format('Y');
+        }
         $cacheKey = 'user_' . Auth::id() . '_aggregates_' . md5(json_encode($request->only(['year_month', 'date', 'description'])));
         $aggregates = Cache::remember($cacheKey, self::TEN_MIN_IN_SEC, function () use ($query, $nowYear) {
             $total_transaction = (clone $query)->count();
@@ -48,10 +53,12 @@ class TransactionsController extends Controller
                 ->having('year', $nowYear)
                 ->orderBy('month')
                 ->get();
-            if ($monthly_expenses->count() > 2) {
-                $percent_change = (new PercentChangeForecast())->changePercent($monthly_expenses->last() ?? self::BASE_AMOUNT, $monthly_expenses->reverse()->skip(1)->first() ?? self::BASE_AMOUNT);
-            }
 
+            if ($monthly_expenses->count() > 2) {
+                $percentChangeForecast = (new PercentChangeForecast());
+                $percent_change = $percentChangeForecast->changePercent($monthly_expenses->last() ?? self::BASE_AMOUNT, $monthly_expenses->reverse()->skip(1)->first() ?? self::BASE_AMOUNT);
+                $percent_changes = collect($percentChangeForecast->changesPercent($monthly_expenses));
+            }
             $top_expenses = (clone $query)->selectRaw("description , COUNT(*) as total,SUM(debit) as debit_sum")
                 ->where('credit', self::BASE_AMOUNT)
                 ->groupBy('description')
@@ -82,11 +89,13 @@ class TransactionsController extends Controller
                 ->groupBy('tag')
                 ->orderByDesc('debit_sum')
                 ->get();
+
             return [
                 'total_transaction' => $total_transaction,
                 'total_spent' => $total_spent,
                 'total_income' => $total_income,
                 'percent_change' => $percent_change ?? self::BASE_AMOUNT,
+                'percent_changes' => $percent_changes ?? collect([]),
                 'monthly_expenses' => $monthly_expenses,
                 'top_expenses' => $top_expenses,
                 'linear_regression_forecast' => round($linear_regression_forecast ?? 0),
@@ -103,6 +112,7 @@ class TransactionsController extends Controller
             'total_spent' => $aggregates['total_spent'],
             'total_income' => $aggregates['total_income'],
             'percent_change' => $aggregates['percent_change'],
+            'percent_changes' => $aggregates['percent_changes'],
             'top_expenses' => $aggregates['top_expenses'],
             'monthly_expenses' => $aggregates['monthly_expenses'],
             'tag_related_spendings' => $aggregates['tag_related_spendings'],
