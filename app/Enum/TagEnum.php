623b746417f534200c7267942deb144cc88c7b85
rupan dangol <rupandangol87@gmail.com>
2025-09-07 13:02:59 +0545

    feat: added Others tags #76

--- app/Enum/TagEnum.php
+++ app/Enum/TagEnum.php
@@ -6,6 +6,11 @@ enum TagEnum: string
 {
     case BILL_SHARING = 'bill_sharing';
     case FAMILY_EXPENSES = 'family_expenses';
+    case FOOD_AND_DRINK = 'food_and_drink';
+    case ENTERTAINMENT = 'entertainment';
+    case UTILITIES = 'utilities';
+    case TRAVEL = 'travel';
+    case SHOPPING = 'shopping';
     case GROCERIES = 'groceries';
     case LEND = 'lend';
     case PERSONAL_USE = 'personal_use';
